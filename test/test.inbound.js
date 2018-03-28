2ebdd2b8131e83ca9b261e39872541e7f8a583d5
Pawel Piotrowski <pawel@koffeecup.net>
2018-03-28 04:08:34 +0100

    patch version 0.0.7 add test, mocha chai sinon, add unit test for inbound module

--- test/test.inbound.js
+++ test/test.inbound.js
@@ -1,20 +1,55 @@
 'use strict';
 
-
+const config = require('../config');
 const Inbound = require('../lib/inbound.js');
 const expect = require('chai').expect;
-
-const InboundInstance = new Inbound({ 
-    recipient: { id: '202775603642477' },
-    timestamp: 1522001779718,
-    sender: { id: '1841093565925442' },
-    postback: { payload: 'get_started_hook', title: 'Get Started' } 
-});
+const sinon = require('sinon');
+const DB = require('../lib/db');
+const knexCleaner = require('knex-cleaner');
 
 describe('Inbound module', () => {
+    
+    const InboundInstance = new Inbound({ 
+        recipient: { id: '987654321' },
+        timestamp: 1522001779718,
+        sender: { id: '123456789' },
+        postback: { payload: 'get_started_hook', title: 'Get Started' } 
+    });
+
     describe('processMessagingItem', () => {
-        it('should export a function', () => {
-            expect(InboundInstance.processMessagingItem).to.be.a('function');
-        })
-    })
+        it('should produce inbound item from messaging item', () => {
+            function testInbound() {
+                const inboundItem = InboundInstance.getInboundItem();
+                expect(inboundItem.type).to.equal('postback');
+                expect(inboundItem.threadKey).to.equal('get_started_hook');
+                expect(inboundItem.cargo).to.deep.equal({
+                    payload: 'get_started_hook', title: 'Get Started'
+                });
+                stubSyncSenderData.restore();
+                stubRoutine.restore();
+            }
+            // stub sync sender data
+            const stubSyncSenderData = sinon.stub(InboundInstance, 'syncSenderData').callsFake(() => {
+                return new Promise(resolve => {
+                    resolve();
+                });
+            });
+            // replace routine with callback to check inbound item
+            const stubRoutine = sinon.stub(InboundInstance, 'routine').callsFake(testInbound);
+            // trigger
+            InboundInstance.processMessagingItem();
+        });
+    });
+
+    describe('syncSenderData', () => {
+        it('should create user data with fallback to default language', async () => {
+            // stub sync sender data
+            await InboundInstance.syncSenderData();
+            const testUsr = await DB('users').where({ fbid: '123456789' });
+            expect(testUsr[0].fbid).to.be.equal('123456789');
+            expect(testUsr[0].dialog_name).to.be.equal(config.defaultDialog);
+            expect(testUsr[0].lang).to.be.equal(config.locales[0]);
+            await knexCleaner.clean(DB);
+        });
+    });
 });
